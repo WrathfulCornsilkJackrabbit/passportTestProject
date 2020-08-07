f6b705f1e8bad11319f6c9eb4bc7ca984dbb234f
alexiorodrigues <alexiorodrigues@gmail.com>
2020-08-07 10:34:55 +0100

    ADD register feature

--- server.js
+++ server.js
@@ -2,12 +2,20 @@
  * Imports
  */
 const express = require('express');
+const bcrypt = require('bcrypt');
 
 /**
  * Global declarations
  */
 const app = express();
 const users = [];
+
+/**
+ * Server Settings
+ */
+app.set('view-engine', 'ejs');
+app.listen(3000);
+app.use(express.urlencoded({ extended: false }));
 
 /**
  * Server Routes
@@ -26,13 +34,19 @@ app.post('/login', (req, res) => {
 app.get('/register', (req, res) => {
   res.render('register.ejs');
 });
-app.post('/register', (req, res) => {
-  req.body.name;
-});
+app.post('/register', async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+    res.redirect('/login');
+  } catch {
+    res.redirect('/register');
+  }
 
-/**
- * Server Settings
- */
-app.set('view-engine', 'ejs');
-app.use(express.urlencoded({ extended: false }));
-app.listen(3000);
+  console.log(users);
+});
