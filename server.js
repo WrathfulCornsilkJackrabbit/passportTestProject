9b4162afcd2995c64118949d3b4c7338a1d8905e
alexiorodrigues <alexiorodrigues@gmail.com>
2020-08-07 10:17:42 +0100

    Create Login and Register forms

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const express = require('express');
  * Global declarations
  */
 const app = express();
+const users = [];
 
 /**
  * Server Routes
@@ -14,15 +15,24 @@ const app = express();
 app.get('/', (req, res) => {
   res.render('index.ejs', { name: 'Alexio' });
 });
+
 app.get('/login', (req, res) => {
   res.render('login.ejs');
 });
+app.post('/login', (req, res) => {
+  console.log('Login successfully');
+});
+
 app.get('/register', (req, res) => {
   res.render('register.ejs');
+});
+app.post('/register', (req, res) => {
+  req.body.name;
 });
 
 /**
  * Server Settings
  */
 app.set('view-engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
 app.listen(3000);
